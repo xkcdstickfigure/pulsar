706b18b463d37b9f2d2063d3455cfa1f07fc8944
Archie Baer <archie@abaer.dev>
2020-10-18 01:00:28 +0100

    Removed example items

--- query/index.js
+++ query/index.js
@@ -9,9 +9,6 @@ module.exports = async (query) => {
     await require(`./${handlers[i]}`)(query, data);
   }
 
-  for (let i = 0; i < Math.floor(Math.random() * 5) + 2; i++)
-    data.items.push({ text: "test" });
-
   data.items = data.items.slice(0, 10);
   return data;
 };
