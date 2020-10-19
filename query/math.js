256033eff36495ee8ef47df94a75a1f6dcd4f7c7
Archie Baer <archie@abaer.dev>
2020-10-19 19:49:13 +0100

    Improved math handler

--- query/math.js
+++ query/math.js
@@ -1,14 +1,19 @@
 const { evaluate } = require("mathjs");
 
 module.exports = (query, data) => {
+  let answer;
+
   try {
     const result = evaluate(query.replace(/x/g, "*"));
     if (typeof result === "number") {
-      data.answer = result.toString();
+      answer = result.toString();
     } else if (typeof result === "string") {
-      data.answer = result;
+      answer = result;
     }
-
-    if (data.answer === "69") data.answer += " (nice)";
   } catch (e) {}
+
+  if (answer === query) return;
+  if (answer === "69") answer += " (nice)";
+
+  data.answer = answer;
 };
