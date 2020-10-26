0022a97382cd650c9e7c2f328e498048e0570db4
Archie Baer <archie@abaer.dev>
2020-10-26 21:15:11 +0000

    Config path

--- query/info.js
+++ query/info.js
@@ -1,7 +1,8 @@
 const { app } = require("electron");
+const paths = require("env-paths")("pulsar", { suffix: "" });
 
 module.exports = (query, data) => {
-  if (query === "version") {
+  if (query === "version")
     data.answer = `You're running Pulsar v${app.getVersion()}`;
-  }
+  else if (query === "config") data.answer = paths.config;
 };
