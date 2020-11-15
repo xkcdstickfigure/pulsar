a76f29e2a79352127816369e88c17345a3d5baf6
Archie Baer <archie@abaer.dev>
2020-11-15 15:17:16 +0000

    Removed grey border, added image gradient

--- src/article.js
+++ src/article.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 export default ({ title, source, url, image }) => (
-  <a href={url} target="_blank" rel="noopener noreferrer" className="article">
-    <div
-      style={{
-        backgroundImage: `url('${image}')`,
-      }}
-    />
-    <div>
-      <h1 className="title">{title}</h1>
-      <h2 className="source">{source}</h2>
+  <a href={url} target="_blank" rel="noopener noreferrer">
+    <div className="article">
+      <div
+        style={{
+          backgroundImage: `url('${image}')`,
+        }}
+        className="image"
+      >
+        <div />
+      </div>
+      <div className="info">
+        <h1 className="title">{title}</h1>
+        <h2 className="source">{source}</h2>
+      </div>
     </div>
   </a>
 );
