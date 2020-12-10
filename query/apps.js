ac505ffd87f9e215163dfc402e4918c3f0773367
Archie Baer <archie@abaer.dev>
2020-12-10 20:16:27 +0000

    App search now case insensitive

--- query/apps.js
+++ query/apps.js
@@ -4,7 +4,10 @@ const leven = require("leven");
 module.exports = async (query, data) => {
   data.items = data.items.concat(
     (await getApps())
-      .map((app) => ({ ...app, leven: leven(app.name, query) }))
+      .map((app) => ({
+        ...app,
+        leven: leven(app.name.toLowerCase(), query.toLowerCase()),
+      }))
       .filter((app) => app.leven / app.name.length <= 0.5)
       .slice(0, 2)
       .sort((a, b) => a.leven - b.leven)
